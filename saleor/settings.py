3186f00fbe6170ff0d92cde918a9f21f58075b80
Chatchai Naenudorn <chatchai@pace6.tech>
2021-03-08 18:25:28 +0700

    remove hardcode ALLOWED_HOSTS

--- saleor/settings.py
+++ saleor/settings.py
@@ -49,8 +49,6 @@ MANAGERS = ADMINS
 
 _DEFAULT_CLIENT_HOSTS = "localhost,127.0.0.1"
 
-ALLOWED_HOSTS = ["localhost", "94541ddda662.ngrok.io", "127.0.0.1"]
-
 ALLOWED_CLIENT_HOSTS = os.environ.get("ALLOWED_CLIENT_HOSTS")
 if not ALLOWED_CLIENT_HOSTS:
     if DEBUG:
